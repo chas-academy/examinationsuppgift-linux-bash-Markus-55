298b4b6acfefde58012e71023ebc06f6572a92a2
Markus-55 <69858432+Markus-55@users.noreply.github.com>
2026-05-13 21:00:54 +0000

    feat: set owner-only permissions and assign folder ownership to user

--- create_users.sh
+++ create_users.sh
@@ -1,24 +1,30 @@
 #!/bin/bash 
-# Operating system uses shebang to understand which interpreter should run the script
+# The shebang tells the system to run this script with Bash
 
 
 # Checks if the user is root
-if test $UID -ne 0; then
+if test "$UID" -ne 0; then
     # Print failure message and stop the script 
     echo "You do not have root access."
     # Exit with status 1 to indicate failure status
     exit 1
 fi
 
-# Loop through each username argument and handle users one by one
-addUser() {
+addUser() {  
+    # Loop through each username argument and handle users one by one
     for user in "$@"; do
-        echo "Created user: $user"
 	# Create a new user and user's home directory with -m
-	useradd -m "$user"
+	useradd -m "$user"	
+        echo "Created user: $user"
+
 	# Create directories inside the user's home directory
-	# Use -p to create missing parent directories if needed 
-	mkdir -p "/home/$user/Documents" "/home/$user/Downloads" "/home/$user/Work"
+	# Use -p to create missing parent directories if needed
+	# Set permissions so only the owner has read, write and execute permissions 
+	mkdir -p -m "u=rwx,g=,o=" "/home/$user/Documents" "/home/$user/Downloads" "/home/$user/Work"
+	# Set user as the owner of the directories instead of root
+	# No group is specified after :, so the user's default group is used
+	chown "$user:" "/home/$user/Documents" "/home/$user/Downloads" "/home/$user/Work"
     done
 }
+# Create new users from the provided usernames
 addUser "$@"
