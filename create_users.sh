d56a8ac789b8d7362db8351a14a73085595a92cd
Markus-55 <69858432+Markus-55@users.noreply.github.com>
2026-05-14 18:45:53 +0000

    feat: add welcome message and user list

--- create_users.sh
+++ create_users.sh
@@ -21,9 +21,17 @@ addUser() {
 	# Use -p to create missing parent directories if needed
 	# Set permissions so only the owner has read, write and execute permissions 
 	mkdir -p -m "u=rwx,g=,o=" "/home/$user/Documents" "/home/$user/Downloads" "/home/$user/Work"
-	# Set user as the owner of the directories instead of root
+	# Write the welcome message into welcome.txt
+	# Create a welcome.txt file if it does exist
+	echo "Välkommen $user" > "/home/$user/welcome.txt"
+	# Finds normal user accounts by checking for UIDs from 1000 to 60000 and excludes the current user
+	# -F ':' tells awk to split each /etc/passwd line into separate fields using ':' as the separator
+	# Append the filtered list of other normal users to the welcome file
+       	awk -F ':' -v currentUser="$user" '$3 >= 1000 && $3 <= 60000 && $1 != currentUser { print $1 }' "/etc/passwd" >> "/home/$user/welcome.txt"
+ 
+	# Set user as the owner of the created directories and welcome file
 	# No group is specified after :, so the user's default group is used
-	chown "$user:" "/home/$user/Documents" "/home/$user/Downloads" "/home/$user/Work"
+	chown "$user:" "/home/$user/Documents" "/home/$user/Downloads" "/home/$user/Work" "/home/$user/welcome.txt"
     done
 }
 # Create new users from the provided usernames
